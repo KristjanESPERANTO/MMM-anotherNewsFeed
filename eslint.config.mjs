74462bc3e06b9482a934aac7a5c2e04f7d78569e
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-07-05 20:06:10 +0200

    chore: add stylistic ESLint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,41 @@
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import markdown from "@eslint/markdown";
+import css from "@eslint/css"
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import js from "@eslint/js"
+import markdown from "@eslint/markdown"
+import stylistic from "@stylistic/eslint-plugin"
 
 export default defineConfig([
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-  { files: ["**/*.{js,mjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-]);
+  {
+    files: ["**/*.css"],
+    language: "css/css",
+    extends: [css.configs.recommended],
+  },
+  {
+    files: ["**/*.{js,mjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    extends: [js.configs.recommended, stylistic.configs.recommended],
+    rules: {
+      "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "script" },
+  },
+  {
+    files: ["**/*.mjs"],
+    languageOptions: { sourceType: "module" },
+  },
+  {
+    files: ["**/*.md"],
+    extends: [markdown.configs.recommended],
+    language: "markdown/gfm",
+  },
+])
